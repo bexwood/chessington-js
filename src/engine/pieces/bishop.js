5da88a0e8cc8fcbabe0386f07f6344caea53883e
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-27 12:59:36 +0100

    pushing before lunch- working on bishop jumping over other pieces

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import GameSettings from "../gameSettings";
 import Square from "../square";
+import {list} from "mocha/lib/reporters";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -16,14 +17,30 @@ export default class Bishop extends Piece {
             };
             let difference = col - currentPosition.col;
             let rowMinusDifference = currentPosition.row - difference;
-            if ((rowMinusDifference >= 0) && (rowMinusDifference <= GameSettings.BOARD_SIZE)) {
+            if ((rowMinusDifference >= 0) && (rowMinusDifference < GameSettings.BOARD_SIZE)) {
                 availableMoves.push(new Square(rowMinusDifference, col));
             };
             let rowPlusDifference = currentPosition.row + difference;
-            if ((rowPlusDifference >= 0) && (rowPlusDifference <= GameSettings.BOARD_SIZE)) {
+            if ((rowPlusDifference >= 0) && (rowPlusDifference < GameSettings.BOARD_SIZE)) {
                 availableMoves.push(new Square(rowPlusDifference, col));
             };
         };
+        availableMoves = this.removeInvalidMoves(board, availableMoves)
         return availableMoves;
     };
+
+    removeInvalidMoves(board, listOfMoves){
+        for (let move in listOfMoves) {
+            if (board.getPiece(listOfMoves[move])) {
+                listOfMoves.splice(move, 1)
+                let diagonalDirection = this.getDiagonalDirection(listOfMoves[move])
+                //need to go on to delete all moves after this move which follow the same direction
+            }
+        }
+        return listOfMoves
+    }
+
+    getDiagonalDirection(position){
+        //implement function to find the direction from 'this' which another position is
+    }
 };
