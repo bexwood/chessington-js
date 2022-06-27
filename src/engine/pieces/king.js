303553ebf4659fe88683678fba50796eba207117
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-27 15:04:16 +0100

    prevented king, knight and pawn going off board

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,5 +1,6 @@
 import Piece from './piece';
 import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class King extends Piece {
     constructor(player) {
@@ -17,6 +18,7 @@ export default class King extends Piece {
         availableMoves.push(new Square(currentPosition.row - 1, currentPosition.col));
         availableMoves.push(new Square(currentPosition.row, currentPosition.col + 1));
         availableMoves.push(new Square(currentPosition.row, currentPosition.col - 1));
+        availableMoves = availableMoves.filter(move => (move.row>=0 && move.row<GameSettings.BOARD_SIZE && move.col>=0 && move.col<GameSettings.BOARD_SIZE))
         return availableMoves;
     }
 }
--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -12,20 +12,25 @@ export default class Pawn extends Piece {
         let currentPosition = board.findPiece(this);
         let allMoves = [];
         if (this.player === Player.WHITE) {
-            if (!board.getPiece(new Square(currentPosition.row + 1, currentPosition.col))) {
-                allMoves.push(new Square(currentPosition.row + 1, currentPosition.col));
-                if (currentPosition.row === 1) {
-                    allMoves.push(new Square(currentPosition.row + 2, currentPosition.col));
+            if (currentPosition.row < (GameSettings.BOARD_SIZE - 1)) {
+                if (!board.getPiece(new Square(currentPosition.row + 1, currentPosition.col))) {
+                    allMoves.push(new Square(currentPosition.row + 1, currentPosition.col));
+                    if (currentPosition.row === 1) {
+                        allMoves.push(new Square(currentPosition.row + 2, currentPosition.col));
+                    }
                 }
             }
         } else {
-            if (!board.getPiece(new Square(currentPosition.row - 1, currentPosition.col))) {
-                allMoves.push(new Square(currentPosition.row - 1, currentPosition.col));
-                if (currentPosition.row === (GameSettings.BOARD_SIZE - 2)) {
-                    allMoves.push(new Square(currentPosition.row - 2, currentPosition.col));
+            if (currentPosition.row > (0)) {
+                if (!board.getPiece(new Square(currentPosition.row - 1, currentPosition.col))) {
+                    allMoves.push(new Square(currentPosition.row - 1, currentPosition.col));
+                    if (currentPosition.row === (GameSettings.BOARD_SIZE - 2)) {
+                        allMoves.push(new Square(currentPosition.row - 2, currentPosition.col));
+                    }
                 }
             }
         }
+
         for (var move in allMoves) {
             if (board.getPiece(allMoves[move])) {
                 allMoves.splice(move, 1);
