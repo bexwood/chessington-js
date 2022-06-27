17ad2999f957fe9ed35e36ae3ed11773acd1f7ea
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-27 15:37:02 +0100

    added function to check if square is empty

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,3 +1,6 @@
+import GameSettings from "../gameSettings";
+import Square from "../square";
+
 export default class Piece {
     constructor(player) {
         this.player = player;
@@ -6,6 +9,33 @@ export default class Piece {
 
     getAvailableMoves(board) {
         throw new Error('This method must be implemented, and return a list of available moves');
+    }
+
+    getLateralMovements(board, square) {
+        let availableMoves = [];
+        for (let i=0; i<GameSettings.BOARD_SIZE; i++){
+            if (square.row !== i && !this.isSquareOccupied(board, i, square.col)){
+                availableMoves.push(new Square(i, square.col));
+            } else if (square.row !== i && this.isSquareOccupied(board, i, square.col)){
+                break;
+            } else {
+                continue;
+            }
+        }
+        for (let i=0; i<GameSettings.BOARD_SIZE; i++){
+            if (square.col !== i && !this.isSquareOccupied(board, square.row, i)){
+                availableMoves.push(new Square(square.row, i));
+            } else if (square.col !== i && this.isSquareOccupied(board, square.row, i)){
+                break;
+            } else {
+                continue;
+            }
+        }
+        return availableMoves;
+    }
+
+    isSquareOccupied(board, row, column){
+        return board.getPiece(new Square(row, column))
     }
 
     moveTo(board, newSquare) {
