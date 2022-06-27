59d72b0d794338ee640acf33f9659494b7e8eb3f
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-27 12:20:41 +0100

    prevented pawn moving if blocking piece infront of it

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Square from "../square";
 import GameSettings from '../gameSettings';
 import Player from "../player";
+import player from "../player";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -10,18 +11,27 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let currentPosition = board.findPiece(this);
-        let availableMoves = [];
-        if (this.player === Player.WHITE){
-            availableMoves.push(new Square(currentPosition.row + 1, currentPosition.col));
-            if (currentPosition.row === 1){
-                availableMoves.push(new Square(currentPosition.row + 2, currentPosition.col));
+        let allMoves = [];
+        if (this.player === Player.WHITE) {
+            if (!board.getPiece(new Square(currentPosition.row + 1, currentPosition.col))) {
+                allMoves.push(new Square(currentPosition.row + 1, currentPosition.col));
+                if (currentPosition.row === 1) {
+                    allMoves.push(new Square(currentPosition.row + 2, currentPosition.col));
+                }
             }
         } else {
-            availableMoves.push(new Square(currentPosition.row - 1, currentPosition.col));
-            if (currentPosition.row === (GameSettings.BOARD_SIZE - 2)){
-                availableMoves.push(new Square(currentPosition.row - 2, currentPosition.col));
+            if (!board.getPiece(new Square(currentPosition.row - 1, currentPosition.col))) {
+                allMoves.push(new Square(currentPosition.row - 1, currentPosition.col));
+                if (currentPosition.row === (GameSettings.BOARD_SIZE - 2)) {
+                    allMoves.push(new Square(currentPosition.row - 2, currentPosition.col));
+                }
             }
         }
-        return availableMoves;
+        for (var move in allMoves) {
+            if (board.getPiece(allMoves[move])) {
+                allMoves.splice(move, 1);
+            }
+        }
+        return allMoves;
     }
 }
