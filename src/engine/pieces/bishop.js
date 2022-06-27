0e6b0eb0fbc54e5514558f221791a3e41598d8ba
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-27 11:38:40 +0100

    allowed bishop movements

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import GameSettings from "../gameSettings";
+import Square from "../square";
 
 export default class Bishop extends Piece {
     constructor(player) {
@@ -6,6 +8,22 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
-    }
-}
+        let currentPosition = board.findPiece(this);
+        let availableMoves = [];
+        for (let col = 0; col < GameSettings.BOARD_SIZE; col++) {
+            if (col === currentPosition.col) {
+                continue;
+            };
+            let difference = col - currentPosition.col;
+            let rowMinusDifference = currentPosition.row - difference;
+            if ((rowMinusDifference >= 0) && (rowMinusDifference <= GameSettings.BOARD_SIZE)) {
+                availableMoves.push(new Square(rowMinusDifference, col));
+            };
+            let rowPlusDifference = currentPosition.row + difference;
+            if ((rowPlusDifference >= 0) && (rowPlusDifference <= GameSettings.BOARD_SIZE)) {
+                availableMoves.push(new Square(rowPlusDifference, col));
+            };
+        };
+        return availableMoves;
+    };
+};
