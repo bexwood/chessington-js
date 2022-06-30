90e0039ef448621966a4cbe0dbc0c8a571b4343d
Rebecca Wood <Rebecca.Wood@softwire.com>
2022-06-30 15:18:41 +0100

    allowed pawn to take via diagonal

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -21,16 +21,21 @@ export default class Pawn extends Piece {
         if (this.firstMove && board.checkSquareOnBoard(currentPosition.row + this.direction, currentPosition.col) && !board.getPiece(new Square(currentPosition.row + this.direction, currentPosition.col))) {
             availableMoves.push(new Square(currentPosition.row + (2*this.direction), currentPosition.col));
         }
-        //let diagonalOpposition = this.checkForOpposites(currentPosition, board);
         availableMoves = this.checkAllMovesOnBoard(availableMoves, board)
-        availableMoves = this.checkAllMovesAvailable(availableMoves, board);
+        availableMoves = this.checkAllMovesAvailable(availableMoves, board)
+        let diagonalOpposition = this.checkForOppositesOnDiagonal(currentPosition, board);
+        availableMoves = availableMoves.concat(diagonalOpposition)
         return availableMoves;
     }
 
-    // checkForOpposites(currentPosition, board){
-    //     let availableMoves = []
-    //     if (board.getPiece(new Square(currentPosition.row+1, currentPosition.col+1)) && board.getPiece(new Square(currentPosition.row+1, currentPosition.col+1)).player !== this.player){
-    //         availableMoves.push(new Square(currentPosition.row+1, currentPosition.col+1))
-    //     }
-    // }
+    checkForOppositesOnDiagonal(currentPosition, board){
+        let availableMoves = []
+        if (board.checkSquareOnBoard(currentPosition.row+this.direction, currentPosition.col+1) && board.getPiece(new Square(currentPosition.row+this.direction, currentPosition.col+1)) && board.getPiece(new Square(currentPosition.row+this.direction, currentPosition.col+1)).player !== this.player && board.getPiece(new Square(currentPosition.row+this.direction, currentPosition.col+1)).constructor.name !== 'King'){
+            availableMoves.push(new Square(currentPosition.row+this.direction, currentPosition.col+1));
+        }
+        if (board.checkSquareOnBoard(currentPosition.row+this.direction, currentPosition.col-1) && board.getPiece(new Square(currentPosition.row+this.direction, currentPosition.col-1)) && board.getPiece(new Square(currentPosition.row+this.direction, currentPosition.col-1)).player !== this.player && board.getPiece(new Square(currentPosition.row+this.direction, currentPosition.col-1)).constructor.name !== 'King'){
+            availableMoves.push(new Square(currentPosition.row+this.direction, currentPosition.col-1));
+        }
+        return availableMoves
+    }
 }
